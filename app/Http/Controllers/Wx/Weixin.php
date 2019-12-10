b416056fda07b11d930658b0d3b3316cf1c989a6
sunhao <sunhao@localhost.localdomain>
2019-12-10 11:26:12 +0000

    自动回话1

--- app/Http/Controllers/Wx/Weixin.php
+++ app/Http/Controllers/Wx/Weixin.php
@@ -10,7 +10,7 @@ class Weixin extends Controller
 {
     private $access_token;
     public function __construct(){
-        $this->access_token=$this->GetAccess_Token();
+//        $this->access_token=$this->GetAccess_Token();
     }
     public function GetAccess_Token(){
        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx4fdcb23b1ce7f2c6&secret=24faac13d7af0aa67ddafc442eded79f';
@@ -53,6 +53,19 @@ class Weixin extends Controller
                 $this->GetUserInfo($token,$openid);
             }
         }
+        if($xml_obj->MsgType=='text'){
+                $tousername=$xml_obj->ToUserName;
+                $fromusername=$xml_obj->FromUserName;
+                $createtime=$xml_obj->time();
+                $content=date('Y-m-d H:i:s').'     '.$xml_obj->Content;
+                $textinfo='<xml><ToUserName><![CDATA['.$tousername.']]></ToUserName>
+                                <FromUserName><![CDATA['.$fromusername.']]></FromUserName>
+                                <CreateTime>'.$createtime.'</CreateTime>
+                                <MsgType><![CDATA[text]]></MsgType>
+                                <Content><![CDATA['.$content.']]></Content>
+                           </xml>';
+            echo $textinfo;
+        }
 
 
     }
@@ -67,26 +80,41 @@ class Weixin extends Controller
     }
     public function xmltest(){
 
-        $aaa=json_decode('{"access_token":"28_HeReUttBN2jUb2z5fnuVDE3LZPaoDOODx-hOdxf7ERDe8xZ3-DBuS_0-jLMpnF_ZWSw-0CxCuKNX_7n-BLX4NVEW9piJHptn8XPMVUylm5lfuEIEa2HZ3i7UAS0WBYaAFABGD","expires_in":7200}');
-        $bbb=json_decode('{"errcode":42001,"errmsg":"access_token expired hints: [qIEFuSeNRa-pdVBHA!]"}');
+//        $aaa=json_decode('{"access_token":"28_HeReUttBN2jUb2z5fnuVDE3LZPaoDOODx-hOdxf7ERDe8xZ3-DBuS_0-jLMpnF_ZWSw-0CxCuKNX_7n-BLX4NVEW9piJHptn8XPMVUylm5lfuEIEa2HZ3i7UAS0WBYaAFABGD","expires_in":7200}');
+//        $bbb=json_decode('{"errcode":42001,"errmsg":"access_token expired hints: [qIEFuSeNRa-pdVBHA!]"}');
 
-        $xml_str = '<xml>
-                            <ToUserName><![CDATA[gh_037619b92bc0]]></ToUserName>
-                            <FromUserName><![CDATA[oOWCkwpc0xrL17uauyKckwF4qaKI]]></FromUserName>
-                            <CreateTime>1575961667</CreateTime>
-                            <MsgType><![CDATA[text]]></MsgType>
-                            <Event><![CDATA[subscribe]]></Event>
-                            <EventKey><![CDATA[]]></EventKey>
-                        </xml>';
+        $xml_str = '<xml><ToUserName><![CDATA[gh_037619b92bc0]]></ToUserName>
+                                <FromUserName><![CDATA[oOWCkwpc0xrL17uauyKckwF4qaKI]]></FromUserName>
+                                <CreateTime>1575976464</CreateTime>
+                                <MsgType><![CDATA[text]]></MsgType>
+                                <Content><![CDATA[⊙∀⊙！]]></Content>
+                           </xml>';
         $xml_obj=simplexml_load_string($xml_str);
 
-                $token='28_HeReUttBN2jUb2z5fnuVDE3LZPaoDOODx-hOdxf7ERDe8xZ3-DBuS_0-jLMpnF_ZWSw-0CxCuKNX_7n-BLX4NVEW9piJHptn8XPMVUylm5lfuEIEa2HZ3i7UAS0WBYaAFABGD';
-                $openid=$xml_obj->FromUserName;
+        if($xml_obj->MsgType=='text'){
+            $tousername=$xml_obj->ToUserName;
+            $fromusername=$xml_obj->FromUserName;
+            $createtime=time();
+            $content=date('Y-m-d H:i:s').'     '.$xml_obj->Content;
+            $textinfo='<xml><ToUserName><![CDATA['.$tousername.']]></ToUserName>
+                                <FromUserName><![CDATA['.$fromusername.']]></FromUserName>
+                                <CreateTime>'.$createtime.'</CreateTime>
+                                <MsgType><![CDATA[text]]></MsgType>
+                                <Content><![CDATA['.$content.']]></Content>
+                           </xml>';
+            echo $textinfo;
+        }
 
 
-        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$token.'&openid='.$openid.'&lang=zh_CN';
-        $json_srt=file_get_contents($url);
-        dd(json_decode($json_srt)->errcode);
+
+
+//                $token='28_HeReUttBN2jUb2z5fnuVDE3LZPaoDOODx-hOdxf7ERDe8xZ3-DBuS_0-jLMpnF_ZWSw-0CxCuKNX_7n-BLX4NVEW9piJHptn8XPMVUylm5lfuEIEa2HZ3i7UAS0WBYaAFABGD';
+//                $openid=$xml_obj->FromUserName;
+//
+//
+//        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$token.'&openid='.$openid.'&lang=zh_CN';
+//        $json_srt=file_get_contents($url);
+//        dd(json_decode($json_srt)->errcode);
 
 
     }
