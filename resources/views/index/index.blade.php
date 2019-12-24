e51c94383bcbbfd5e819b3e816da0e0dc0604e27
sunhao <1154503210@qq.com>
2019-12-24 08:21:19 +0000

    手机电脑不一致

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -500,7 +500,7 @@
 <script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 <script>
     wx.config({
-        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: 'wx4fdcb23b1ce7f2c6', // 必填，公众号的唯一标识
         timestamp: '{{$js_sdk['timestamp']}}', // 必填，生成签名的时间戳
         nonceStr: '{{$js_sdk['nonceStr']}}', // 必填，生成签名的随机串
@@ -519,16 +519,16 @@
                 }
             })
         });
-        wx.onMenuShareAppMessage({
-            title: '微商城已经上线', // 分享标题
-            desc: '点击进入', // 分享描述
-            link: 'http://1905sunhao.comcto.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://1905sunhao.comcto.com/storage/images/微信图片_20191118144643.bmp', // 分享图标
-            type: '', // 分享类型,music、video或link，不填默认为
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-                alert('分享商城成功')
-            }
+        wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+            wx.updateAppMessageShareData({
+                title: '瞧一瞧看一看', // 分享标题
+                desc: '元旦促销', // 分享描述
+                link: 'http://1905sunhao.comcto.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1905sunhao.comcto.com/storage/images/微信图片_20191118144643.bmp', // 分享图标
+                success: function () {
+                    // 设置成功
+                }
+            })
         });
 
     });
